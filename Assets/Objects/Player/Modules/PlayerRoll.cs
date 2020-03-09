5a02ecb2dbbb644583d671efaf31520b7c1c98b0
Moe-Baker <moe4baker@gmail.com>
2020-03-09 17:15:01 +0200

    Work

---
+++ Assets/Objects/Player/Modules/PlayerRoll.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+	public class PlayerRoll : Player.Module
+	{
+        [SerializeField]
+        float multiplier;
+
+        public override void Init()
+        {
+            base.Init();
+
+            Player.OnProcess += Process;
+        }
+
+        private void Process()
+        {
+            transform.Rotate(Player.rigidbody.velocity * multiplier);
+        }
+    }
+}
